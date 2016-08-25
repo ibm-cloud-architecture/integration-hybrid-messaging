a7320ee29569bbbd0eef51dcb405c5719b5997e5
edwin van de grift <edwinvandegrift@us.ibm.com>
2016-08-25 08:28:55 -0700

    cleanup and small fixes

--- sample-cf-app-using-mqlight/app.js
+++ sample-cf-app-using-mqlight/app.js
@@ -15,14 +15,15 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+//app.get('/', function (req, res) {
+//  //res.send('GET request to homepage');
+//  console.log('sendClient.state: %s', sendClient.state);
+//});
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
 //
-var mqlight = require('mqlight');
 var mq_server = 'localhost';
 var mq_port = '5672';
 var mq_user = 'user';
@@ -33,33 +34,34 @@ if(appEnv.isLocal == false) {
   mq_user = process.env.MQ_USER;
   mq_password = process.env.MQ_PASSWORD;
 }
-var service = "amqp://" + mq_user + ":" + mq_password
+var myservice = "amqp://" + mq_user + ":" + mq_password
                         + "@" + mq_server + ":" + mq_port;
-var sendClient = mqlight.createClient({service: service});
-var topic = 'public';
-var data = 'Hello World!'
+var mqlight = require('mqlight');
+var sendClient = mqlight.createClient({service: myservice});
 
 sendClient.on('error', function(error) {
     console.error('mqlight.createClient error');
     if (error) {
       if (error.message) console.error('message: %s', error.toString());
       else if (error.stack) console.error(error.stack);
-      sendClient.stop();
     }
 });
 
 app.get('/', function(req, res, next) {
   if(sendClient.state == 'started') {
-    console.log('Connected to %s', sendClient.service);
+    var topic = 'public';
+    var data = 'Hello World!'
     console.log('Sending to: %s, data: %s', topic, data);
     sendClient.send(topic, data);
   }
   next();
 });
 
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-  console.log('isLocal: %s', appEnv.isLocal);
+  console.log('server starting on ' + appEnv.url);
 });
